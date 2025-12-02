81fd9c38d83d5e1d9cefd820ea64955278f0994c
Vivek <vivekchilampil@gmail.com>
2025-12-02 12:57:14 +0530

    Started programming

---
+++ javaprogramming/HelloWorld.java
@@ -0,0 +1,10 @@
+package javaprogramming;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ javaprogramming/TypeProgs.java
@@ -0,0 +1,19 @@
+package javaprogramming;
+
+public class TypeProgs {
+	
+	  public static void main(String args[]) { 
+	    byte b = 50; 
+	    char c = 'a'; 
+	    short s = 1024; 
+	    int i = 1234; 
+	    float f = 7.67f; 
+	    double d = .6921; 
+			
+	    double result = (f / b) - (i / c) + (d * s);  // The Expression 
+			
+	    System.out.println("result = " + result);  //Result after all the promotions are done 
+	  } 
+	} 
+
+
